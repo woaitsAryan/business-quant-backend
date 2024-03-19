92eeedb618ed4e3c1028ba38a3969cd80b8742e6
woaitsAryan <yoprince.ab@gmail.com>
2024-03-19 15:01:59 +0530

    feat: added filter route

--- controllers/find.go
+++ controllers/find.go
@@ -1,20 +1,21 @@
 package controllers
 
 import (
-	"fmt"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/woaitsAryan/business-quant-backend/initializers"
+	"github.com/woaitsAryan/business-quant-backend/models"
 	"github.com/woaitsAryan/business-quant-backend/utils"
 )
 
 func FilterData(c *fiber.Ctx) error {
 
 	db := initializers.DB
+
 	db = utils.GetStockData(db, c)
 
-	fmt.Println(db.Statement.Vars...)
+	var stocks []models.Stock
 
-	fmt.Println("Filtering data...")
-	return nil
+	db.Find(&stocks)
+
+	return c.Status(200).JSON(stocks)
 }
--- utils/filter.go
+++ utils/filter.go
@@ -1,31 +1,32 @@
 package utils
 
 import (
-	"fmt"
+	"strconv"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
-	// "github.com/woaitsAryan/business-quant-backend/helpers"
+	"github.com/woaitsAryan/business-quant-backend/helpers"
 	"gorm.io/gorm"
-	// "strconv"
 	"strings"
-	// "time"
 )
 
 func GetStockData(db *gorm.DB, c *fiber.Ctx) *gorm.DB {
 	ticker := c.Query("ticker")
 	column := c.Query("column")
-	// period := c.Query("period")
-	columns := strings.Split(column, ",")
-	fmt.Println(columns)
-	query := db.Debug().Select(columns).Where("ticker = ?", ticker)
-	fmt.Println(query)
-	// if period != "" {
-	// 	years, err := strconv.Atoi(period)
-	// 	if err != nil {
-	// 		helpers.LogServerError("Error parsing period", err, "utils/filter.go")
-	// 	}
-	// 	startDate := time.Now().AddDate(-years, 0, 0)
-	// 	query = query.Where("date >= ?", startDate)
-	// }
+	period := c.Query("period")
 
+	columns := strings.Split(column, ",")
+	query := db.Select(columns).Where("ticker = ?", ticker)
+
+	if period != "" {
+		yearsStr := strings.TrimSuffix(period, "y")
+		years, err := strconv.Atoi(yearsStr)
+		if err != nil {
+			helpers.LogServerError("Error parsing period", err, "utils/filter.go")
+			return nil
+		}
+		Date := time.Now().AddDate(-years, 0, 0)
+		query = query.Where("date >= ?", Date)
+	}
 	return query
 }
